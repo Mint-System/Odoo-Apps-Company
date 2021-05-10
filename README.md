e464527ea0d276c50295fc5892ad66d3733f6487
Janik von Rotz <login@janikvonrotz.ch>
2021-05-10 14:28:05 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Odoo Apps: Company
+
+Collection of res.company model related modules.
+
+## Usage
+
+Clone module into Odoo addon directory.
+
+```bash
+git clone git@github.com:mint-system/odoo-apps-company.git ./addons/company
+```
+
+## Available modules
+
+| Module                                              | Summary                                |
+| --------------------------------------------------- | -------------------------------------- |
+| [company_registry_footer](company_registry_footer/) | Show field company registry in footer. |
---
+++ company_registry_footer/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Company Registry Footer",
+
+    'summary': """
+        Show field company registry in footer.
+    """,
+
+    'description': """
+        Show field company registry in footer.
+    """,
+
+    'author': "Mint System GmbH",
+    'website': "https://www.mint-system.ch",
+    'category': 'Customizations',
+    'version': '14.0.1.0.0',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/web.external_layout_standard.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+}
